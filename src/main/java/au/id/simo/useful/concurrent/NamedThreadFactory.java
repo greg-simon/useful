bcee1a743134e3d54dfcf93547c163b5d29bb200
Greg Simon <greg@simo.id.au>
2020-10-16 14:54:37 +1100

    Removed thread group handling, as it never really worked right.

--- src/main/java/au/id/simo/useful/concurrent/NamedThreadFactory.java
+++ src/main/java/au/id/simo/useful/concurrent/NamedThreadFactory.java
@@ -11,7 +11,6 @@ public class NamedThreadFactory implements ThreadFactory {
     public final AtomicInteger threadNum;
     public final String namePrefix;
     public final boolean daemon;
-    public ThreadGroup group;
 
     public NamedThreadFactory(String namePrefix) {
         this(namePrefix, false);
@@ -20,38 +19,7 @@ public class NamedThreadFactory implements ThreadFactory {
     public NamedThreadFactory(String namePrefix, boolean daemon) {
         this.threadNum = new AtomicInteger(0);
         this.namePrefix = namePrefix;
-        ThreadGroup mainGroup = getMainThreadGroup();
-        this.group = new ThreadGroup(mainGroup, namePrefix);
-        this.group.setDaemon(daemon);
         this.daemon = daemon;
-    }
-
-    private ThreadGroup getMainThreadGroup() {
-        // find the root group 'system'
-        ThreadGroup system = null;
-        ThreadGroup threadGroup
-                = Thread.currentThread().getThreadGroup();
-        while (threadGroup != null) {
-            system = threadGroup;
-            threadGroup = threadGroup.getParent();
-        }
-        if (system == null) {
-            return null;
-        }
-        // look in system thread group for an array of groups
-        int groupCount = system.activeGroupCount();
-        ThreadGroup[] tgArray = new ThreadGroup[groupCount];
-        system.enumerate(tgArray, false);
-
-        // look in array for 'main'
-        for (ThreadGroup thisGroup : tgArray) {
-            String name = thisGroup.getName();
-            if ("main".equals(name)) {
-                return thisGroup;
-            }
-        }
-
-        return null;
     }
 
     private String getNextThreadName() {
@@ -64,12 +32,7 @@ public class NamedThreadFactory implements ThreadFactory {
 
     @Override
     public Thread newThread(Runnable r) {
-        if (group.isDestroyed()) {
-            // this happens when the group has no threads left
-            ThreadGroup mainGroup = getMainThreadGroup();
-            group = new ThreadGroup(mainGroup, namePrefix);
-        }
-        Thread thread = new Thread(group, r, getNextThreadName());
+        Thread thread = new Thread(r, getNextThreadName());
         if (thread.isDaemon() != daemon) {
             thread.setDaemon(daemon);
         }
