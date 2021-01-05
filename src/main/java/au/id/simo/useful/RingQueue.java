afebd984be3931d12280026efabbcf9e9dd8eaf9
Greg Simon <greg@simo.id.au>
2021-01-05 13:34:08 +1100

    Added RingQueue, a Queue backed by a ring buffer.

---
+++ src/main/java/au/id/simo/useful/RingQueue.java
@@ -0,0 +1,183 @@
+package au.id.simo.useful;
+
+import java.util.AbstractQueue;
+import java.util.Iterator;
+
+/**
+ * A fixed size implementation of {@link Queue}.
+ * <p>
+ * It will always accept new entries as they will silently overwrite existing
+ * entries if the collection is full.
+ * @param <E>
+ */
+public class RingQueue<E> extends AbstractQueue<E> {
+    /**
+     * Flag to indicate the head or tail index pointer is not set.
+     */
+    private static final int INDEX_EMPTY = -1;
+    private final Object[] array;
+    /**
+     * Add index. Points to last written location. Increment then write.
+     */
+    private int head;
+    /**
+     * Remove index. Points to oldest value to read from.
+     */
+    private int tail;
+
+    public RingQueue(int size) {
+        array = new Object[size];
+        head = INDEX_EMPTY;
+        tail = INDEX_EMPTY;
+    }
+    
+    /**
+     * Logic for wrapping indexes on array length.
+     *
+     * @param index head or tail.
+     * @return new incremented value, possibly wrapped back to 0 if required.
+     */
+    protected int incrementIndex(int index) {
+        return incrementIndex(index, 1);
+    }
+
+    protected int incrementIndex(int index, int incrementBy) {
+        return (index + incrementBy) % array.length;
+    }
+    
+    @Override
+    public Iterator<E> iterator() {
+        return new RingQueueIterator();
+    }
+
+    @Override
+    public int size() {
+        if (tail == INDEX_EMPTY) {
+            return 0; // initial empty state.
+        }
+
+        int size;
+        if (tail == head) {
+            size = 0;
+        } else if (tail > head) {
+            size = (array.length + head) - tail;
+        } else {
+            size = head - tail;
+        }
+        return size + 1;
+    }
+
+    /**
+     * Will overwrite oldest entries to fit new entries in.
+     * 
+     * @param e
+     * @return 
+     */
+    @Override
+    public boolean offer(E e) {
+        head = incrementIndex(head);
+        array[head] = e;
+
+        // adjust tail if required.
+        if (tail == INDEX_EMPTY) {
+            tail = head;
+        } else if (tail == head) {
+            tail = incrementIndex(tail);
+        }
+        return true;
+    }
+
+    @Override
+    public E poll() {
+        if (isEmpty()) {
+            return null;
+        }
+        Object t = array[tail];
+        array[tail] = null;
+        if (tail == head) {
+            tail = INDEX_EMPTY;
+            head = INDEX_EMPTY;
+        } else {
+            tail = incrementIndex(tail);
+        }
+        return (E) t;
+    }
+
+    @Override
+    public E peek() {
+        if(isEmpty()) {
+            return null;
+        }
+        return peek(0);
+    }
+    
+    /**
+     * 
+     * @param index Relative index, where 0 means the oldest item in the collection.
+     * @return the value that is {@code index} positions from the oldest item in
+     * the collection.
+     */
+    public E peek(int index) {
+        if (index >= size()) {
+            throw new ArrayIndexOutOfBoundsException(
+                    String.format("Index value %s is larger than the number of elements %s.",
+                             index,
+                             size()
+                    )
+            );
+        }
+        int relindex = incrementIndex(tail, index);
+        return (E) array[relindex];
+    }
+    
+    public int maxSize() {
+        return array.length;
+    }
+    
+    public boolean isNotEmpty() {
+        return !isEmpty();
+    }
+
+    public boolean isFull() {
+        return tail == incrementIndex(head);
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("RingQueue");
+        if (head == INDEX_EMPTY) {
+            sb.append('+');
+        }
+        if (tail == INDEX_EMPTY) {
+            sb.append('-');
+        }
+        sb.append('[');
+        for (int i = 0; i < array.length; i++) {
+            if (head == i) {
+                sb.append('+');
+            }
+            if (tail == i) {
+                sb.append('-');
+            }
+            sb.append(String.valueOf(array[i]));
+            sb.append(',');
+        }
+        sb.replace(sb.lastIndexOf(","), sb.length(), "]");
+        return sb.toString();
+    }
+    
+    private class RingQueueIterator implements Iterator<E> {
+        private int index = 0;
+
+        @Override
+        public boolean hasNext() {
+            return index < size();
+        }
+
+        @Override
+        public E next() {
+            return peek(index++);
+        }
+    }
+}
---
+++ src/test/java/au/id/simo/useful/RingQueueTest.java
@@ -0,0 +1,176 @@
+package au.id.simo.useful;
+
+import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ */
+public class RingQueueTest {
+
+    @Test
+    public void testIncrementIndex() {
+        RingQueue<Integer> rq = new RingQueue(3);
+        assertEquals(1,rq.incrementIndex(0));
+        assertEquals(2,rq.incrementIndex(1));
+        assertEquals(0,rq.incrementIndex(2));
+        assertEquals(1,rq.incrementIndex(3));
+    }
+
+    @Test
+    public void testHappy() {
+        RingQueue<Integer> rq = new RingQueue<>(3);
+        System.out.println(rq);
+        assertEquals(0, rq.size());
+        assertEquals(3, rq.maxSize());
+        assertTrue(rq.isEmpty());
+        assertFalse(rq.isNotEmpty());
+        assertFalse(rq.isFull());
+        
+        rq.add(1);
+        System.out.println(rq);
+        assertEquals(1, rq.size());
+        assertFalse(rq.isEmpty());
+        assertFalse(rq.isFull());
+        assertTrue(rq.isNotEmpty());
+        assertEquals(1, rq.peek());
+        assertArrayEquals(new Integer[]{1}, rq.toArray());
+        
+        rq.add(2);
+        System.out.println(rq);
+        assertEquals(2, rq.size());
+        assertFalse(rq.isEmpty());
+        assertFalse(rq.isFull());
+        assertTrue(rq.isNotEmpty());
+        assertEquals(1, rq.peek());
+        assertArrayEquals(new Integer[]{1,2}, rq.toArray());
+        
+        rq.add(3);
+        System.out.println(rq);
+        assertEquals(3, rq.size());
+        assertFalse(rq.isEmpty());
+        assertTrue(rq.isFull());
+        assertTrue(rq.isNotEmpty());
+        assertEquals(1, rq.peek());
+        assertArrayEquals(new Integer[]{1,2,3}, rq.toArray());
+        
+        rq.add(4);
+        System.out.println(rq);
+        assertEquals(3, rq.size());
+        assertFalse(rq.isEmpty());
+        assertTrue(rq.isNotEmpty());
+        assertTrue(rq.isFull());
+        assertEquals(2, rq.peek());
+        assertArrayEquals(new Integer[]{2,3,4}, rq.toArray());
+        
+        rq.add(5);
+        System.out.println(rq);
+        assertEquals(3, rq.size());
+        assertFalse(rq.isEmpty());
+        assertTrue(rq.isNotEmpty());
+        assertTrue(rq.isFull());
+        assertEquals(3, rq.peek());
+        assertArrayEquals(new Integer[]{3,4,5}, rq.toArray());
+        
+        // removing values
+        assertEquals(3, rq.remove());
+        System.out.println(rq);
+        assertEquals(2, rq.size());
+        assertFalse(rq.isEmpty());
+        assertTrue(rq.isNotEmpty());
+        assertFalse(rq.isFull());
+        assertArrayEquals(new Integer[]{4,5}, rq.toArray());
+        
+        assertEquals(4, rq.remove());
+        System.out.println(rq);
+        assertEquals(1, rq.size());
+        assertFalse(rq.isEmpty());
+        assertTrue(rq.isNotEmpty());
+        assertFalse(rq.isFull());
+        assertArrayEquals(new Integer[]{5}, rq.toArray());
+        
+        assertEquals(5, rq.remove());
+        System.out.println(rq);
+        assertEquals(0, rq.size());
+        assertTrue(rq.isEmpty());
+        assertFalse(rq.isNotEmpty());
+        assertFalse(rq.isFull());
+        assertArrayEquals(new Integer[]{}, rq.toArray());
+        
+        rq.clear();
+        System.out.println(rq);
+    }
+    
+    @Test
+    public void testClearAndRemove() {
+        RingQueue<Integer> rq = new RingQueue<>(5);
+        rq.add(1);
+        rq.add(2);
+        rq.add(3);
+        rq.add(4);
+        rq.add(5);
+        assertEquals(5, rq.size());
+        assertArrayEquals(new Integer[]{1,2,3,4,5}, rq.toArray());
+        
+        rq.clear();
+        assertEquals(0, rq.size());
+        assertTrue(rq.isEmpty());
+        assertArrayEquals(new Integer[]{}, rq.toArray());
+        
+        rq.add(6);
+        rq.add(7);
+        rq.add(8);
+        rq.add(9);
+        rq.add(0);
+        assertTrue(rq.isFull());
+        assertFalse(rq.isEmpty());
+        assertEquals(6,rq.remove());
+        assertEquals(7,rq.remove());
+        assertEquals(8,rq.remove());
+        assertEquals(9,rq.remove());
+        assertEquals(0,rq.remove());
+        assertTrue(rq.isEmpty());
+        assertNull(rq.peek());
+        
+        boolean execeptionThrown = false;
+        try {
+            rq.remove();
+            fail("Expected exception before this line");
+        } catch (NoSuchElementException e) {
+            execeptionThrown = true;
+        }
+        assertTrue(execeptionThrown);
+    }
+
+    @Test
+    public void testGet() {
+        RingQueue<Integer> rb = new RingQueue<>(3);
+        rb.add(1);
+        rb.add(2);
+        rb.add(3);
+        System.out.println(rb);
+        assertEquals(1, rb.peek(0));
+        assertEquals(2, rb.peek(1));
+        assertEquals(3, rb.peek(2));
+        
+        try {
+            rb.peek(3);
+            fail("ArrayIndexOutOfBoundsException should have been thrown above");
+        } catch (ArrayIndexOutOfBoundsException e) {}
+        
+        rb.add(4);
+        rb.add(5);
+        System.out.println(rb);
+        assertEquals(3, rb.peek(0));
+        assertEquals(4, rb.peek(1));
+        assertEquals(5, rb.peek(2));
+        
+        try {
+            rb.peek(3);
+            fail("ArrayIndexOutOfBoundsException should have been thrown above");
+        } catch (ArrayIndexOutOfBoundsException e) {}
+    }
+}
