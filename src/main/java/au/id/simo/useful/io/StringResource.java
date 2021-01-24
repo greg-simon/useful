9f470e0442acf455258f02f3100bc9e113e5eb1a
Greg Simon <greg@simo.id.au>
2021-01-25 09:20:36 +1100

    Made StringResource methods simpler than parent class methods.

--- src/main/java/au/id/simo/useful/io/StringResource.java
+++ src/main/java/au/id/simo/useful/io/StringResource.java
@@ -3,6 +3,9 @@ package au.id.simo.useful.io;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.StringReader;
 
 /**
  * A Resource implementation that holds a String and provides
@@ -18,5 +21,27 @@ public class StringResource extends Resource {
     @Override
     public InputStream inputStream() throws IOException {
         return new ByteArrayInputStream(str.getBytes());
+    }
+
+    @Override
+    public byte[] getBytes() throws IOException {
+        return str.getBytes();
+    }
+
+    @Override
+    public Reader getReader() throws IOException {
+        return new StringReader(str);
+    }
+
+    @Override
+    public String getString() throws IOException {
+        return str;
+    }
+
+    @Override
+    public long copyTo(OutputStream out) throws IOException {
+        byte[] bytes = str.getBytes();
+        out.write(bytes);
+        return bytes.length;
     }
 }
