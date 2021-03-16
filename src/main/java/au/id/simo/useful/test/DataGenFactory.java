415a8c54bb17eae7183d1acf31e5aeb6f49a35f4
Greg Simon <greg@simo.id.au>
2021-03-16 14:56:42 +1100

    New data gen methods.

--- src/main/java/au/id/simo/useful/test/DataGenFactory.java
+++ src/main/java/au/id/simo/useful/test/DataGenFactory.java
@@ -2,6 +2,9 @@ package au.id.simo.useful.test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Random;
+
+import au.id.simo.useful.io.LimitedInputStream;
 
 /**
  * Factory for obtaining test data generators.
@@ -9,7 +12,8 @@ import java.io.InputStream;
 public class DataGenFactory {
     /**
      * 
-     * @return an InputStream that returns starts at zero and counts up for each byte
+     * @return an InputStream that returns starts at zero and counts up for each
+     * byte.
      */
     public static InputStream incrementingBytes() {
         return new InputStream() {
@@ -19,5 +23,29 @@ public class DataGenFactory {
                 return counter++ & 0xff;
             }
         };
+    }
+    
+    /**
+     * 
+     * @param limit The number of bytes to return before ending the stream.
+     * @return an InputStream that returns starts at zero and counts up for each
+     * byte.
+     */
+    public static InputStream incrementingBytesWithLimit(long limit) {
+        return new LimitedInputStream(incrementingBytes(),limit);
+    }
+    
+    public static InputStream randomBytes() {
+        Random random = new Random();
+        return new InputStream() {
+            @Override
+            public int read() throws IOException {
+                return random.nextInt(256);
+            }
+        };
+    }
+    
+    public static InputStream randomBytesWithLimit(long limit) {
+        return new LimitedInputStream(randomBytes(), limit);
     }
 }
