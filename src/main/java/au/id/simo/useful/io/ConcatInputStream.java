931780722afbf20e5805176a036a14c8c7d95749
Greg Simon <greg@simo.id.au>
2021-05-20 10:42:31 +1000

    Minor Concat improvements.

--- src/main/java/au/id/simo/useful/io/ConcatInputStream.java
+++ src/main/java/au/id/simo/useful/io/ConcatInputStream.java
@@ -4,33 +4,55 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Iterator;
-import java.util.List;
 
 /**
- * Joins multiple InputStreams into one.
+ * Reads the provided InputStreams one after the other in the order they are
+ * provided. As if all provided InputStreams are concatenated.
  */
 public class ConcatInputStream extends InputStream {
 
-    private final List<InputStream> inputStreams;
-    private final Iterator<InputStream> readStreamsItr;
+    private final Iterable<InputStream> streams;
+    private final Iterator<InputStream> streamsItr;
     private InputStream currentStream;
 
-    public ConcatInputStream(InputStream... inputStreams) {
-        this.inputStreams = Arrays.asList(inputStreams);
-        this.readStreamsItr = this.inputStreams.iterator();
+    public ConcatInputStream(InputStream... streams) {
+        this(Arrays.asList(streams));
+    }
+    
+    public ConcatInputStream(Iterable<InputStream> streams) {
+        this.streams = streams;
+        this.streamsItr = streams.iterator();
     }
 
+    /**
+     * Obtains the current InputStream, or the next InputStream if the current
+     * InputStream is not set.
+     * <p>
+     * {@code null} is returned to flag there is no further InputStreams
+     * available.
+     *
+     * @return The current InputStream if set, The next InputStream if current
+     * is not set and there is at least one more, or null if the current
+     * InputStream is not set and no further InputStreams are available.
+     */
     private InputStream currentStream() {
         if (currentStream == null) {
-            if (readStreamsItr.hasNext()) {
-                currentStream = readStreamsItr.next();
+            if (streamsItr.hasNext()) {
+                currentStream = streamsItr.next();
             }
         }
         return currentStream;
     }
 
-    private void currentEnded() {
+    /**
+     * Forces the current InputStream to be unset and the next InputStream to be
+     * set to current and returned.
+     *
+     * @return The next InputStream, or {@code null} if none remain.
+     */
+    private InputStream nextStream() {
         currentStream = null;
+        return currentStream();
     }
 
     @Override
@@ -39,8 +61,7 @@ public class ConcatInputStream extends InputStream {
         while (current != null) {
             int returnValue = current.read();
             if (returnValue == -1) {
-                currentEnded();
-                current = currentStream();
+                current = nextStream();
             } else {
                 return returnValue;
             }
@@ -54,8 +75,7 @@ public class ConcatInputStream extends InputStream {
         while (current != null) {
             int readCount = current.read(b, off, len);
             if (readCount == -1) {
-                currentEnded();
-                current = currentStream();
+                current = nextStream();
             } else {
                 return readCount;
             }
@@ -65,7 +85,7 @@ public class ConcatInputStream extends InputStream {
 
     @Override
     public void close() throws IOException {
-        for (InputStream in : inputStreams) {
+        for (InputStream in : streams) {
             in.close();
         }
     }
--- src/main/java/au/id/simo/useful/io/ConcatReader.java
+++ src/main/java/au/id/simo/useful/io/ConcatReader.java
@@ -4,23 +4,37 @@ import java.io.IOException;
 import java.io.Reader;
 import java.util.Arrays;
 import java.util.Iterator;
-import java.util.List;
 
 /**
- * Joins multiple Readers into one.
+ * Reads the provided Readers one after the other in the order they are
+ * provided. As if all provided Readers are concatenated.
  */
 public class ConcatReader extends Reader {
 
-    private final List<Reader> readers;
+    private final Iterable<Reader> readers;
     private final Iterator<Reader> readerItr;
     private Reader currentReader;
 
     public ConcatReader(Reader... readers) {
-        this.readers = Arrays.asList(readers);
-        this.readerItr = this.readers.iterator();
+        this(Arrays.asList(readers));
     }
 
-    private Reader currentStream() {
+    public ConcatReader(Iterable<Reader> readers) {
+        this.readers = readers;
+        this.readerItr = readers.iterator();
+    }
+
+    /**
+     * Obtains the current Reader, or the next Reader if the current Reader is
+     * not set.
+     * <p>
+     * {@code null} is returned to flag there is no further readers available.
+     *
+     * @return The current Reader if set, The next Reader if current is not set
+     * and there is at least one more, or null if the current Reader is not set
+     * and no further Readers are available.
+     */
+    private Reader currentReader() {
         if (currentReader == null) {
             if (readerItr.hasNext()) {
                 currentReader = readerItr.next();
@@ -29,18 +43,24 @@ public class ConcatReader extends Reader {
         return currentReader;
     }
 
-    private void currentEnded() {
+    /**
+     * Forces the current Reader to be unset and the next Reader to be set to
+     * current and returned.
+     *
+     * @return The next Reader, or {@code null} if none remain.
+     */
+    private Reader nextReader() {
         currentReader = null;
+        return currentReader();
     }
 
     @Override
     public int read() throws IOException {
-        Reader current = currentStream();
+        Reader current = currentReader();
         while (current != null) {
             int returnValue = current.read();
             if (returnValue == -1) {
-                currentEnded();
-                current = currentStream();
+                current = nextReader();
             } else {
                 return returnValue;
             }
@@ -50,12 +70,11 @@ public class ConcatReader extends Reader {
 
     @Override
     public int read(char[] b, int off, int len) throws IOException {
-        Reader current = currentStream();
+        Reader current = currentReader();
         while (current != null) {
             int readCount = current.read(b, off, len);
             if (readCount == -1) {
-                currentEnded();
-                current = currentStream();
+                current = nextReader();
             } else {
                 return readCount;
             }
