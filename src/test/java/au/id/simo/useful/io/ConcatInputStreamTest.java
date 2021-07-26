b20aa9b7c7a980d9001104624dfbd1863792b4eb
Greg Simon <greg@simo.id.au>
2021-07-26 16:49:15 +1000

    ConcatInputStream test now inherits all the generic InputStream tests.

--- src/test/java/au/id/simo/useful/io/ConcatInputStreamTest.java
+++ src/test/java/au/id/simo/useful/io/ConcatInputStreamTest.java
@@ -3,21 +3,22 @@ package au.id.simo.useful.io;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
 /**
  *
  */
-public class ConcatInputStreamTest {
-    /**
-     * Test of getByteCount method, of class SizeCounterInputStream.
-     *
-     * @throws java.io.IOException
-     */
+public class ConcatInputStreamTest implements InputStreamTest {
+
+    @Override
+    public InputStream create(InputStream in) {
+        return new ConcatInputStream(in);
+    }
+
     @Test
     public void testRead() throws IOException {
         ConcatInputStream cin = new ConcatInputStream(
@@ -26,7 +27,7 @@ public class ConcatInputStreamTest {
         );
         ByteArrayOutputStream bout = new ByteArrayOutputStream();
         int i;
-        while((i=cin.read())!=-1) {
+        while ((i = cin.read()) != -1) {
             bout.write(i);
         }
         assertEquals("Hello there", new String(bout.toByteArray()));
@@ -45,8 +46,8 @@ public class ConcatInputStreamTest {
         );
         ByteArrayOutputStream bout = new ByteArrayOutputStream();
         int i;
-        byte[] buf = new byte[1024];
-        while((i=cin.read(buf, 0, buf.length))!=-1) {
+        byte[] buf = new byte[2];
+        while ((i = cin.read(buf, 0, buf.length)) != -1) {
             bout.write(buf, 0, i);
         }
         assertEquals("Hello there", new String(bout.toByteArray()));
