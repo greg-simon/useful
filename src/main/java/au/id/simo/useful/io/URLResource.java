bdcca105516ee58aa0c8b5d063f7202537dc8796
Greg Simon <greg@simo.id.au>
2021-09-01 10:46:02 +1000

    urlString is now set no matter what constructor is used.

--- src/main/java/au/id/simo/useful/io/URLResource.java
+++ src/main/java/au/id/simo/useful/io/URLResource.java
@@ -22,8 +22,8 @@ public class URLResource implements Resource {
     }
 
     public URLResource(URL url) {
+        this.urlString = url.toExternalForm();
         this.url = url;
-        this.urlString = null;
     }
 
     @Override
